597866021bd0dd71abf937589c2d604b54fba787
Asitha <asitha.muthumala@paraqum.com>
2023-07-21 22:22:26 +0530

    Initial Commit

---
+++ src/components/home/works.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Modal, Button } from "antd";
+
+class AppWorks extends React.Component {
+    state = { visible: false };
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    render() {
+        return(
+            <div id="works" className="block worksBlock">
+                <div className="container-fluid">
+                    <div className="titleHolder">
+                        <h2>How it works</h2>
+                        <p>It can work simply by creating accessories while doing your task</p>
+                    </div>
+                    <div className="contentHolder">
+                        <Button onClick={this.showModal}>
+                            <i className="fas fa-play"></i>
+                        </Button>
+                    </div>
+                    <Modal
+                    title="Tech Tutorial"
+                    open={this.state.visible}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                    >
+                        <iframe title="Tech Tutorial" width="100%" height="350" src="https://www.youtube.com/embed/8f8_JYIzOno?list=PLiUrl-SQRR7LQINGQGE99pXWDuKq4SxfU"></iframe>
+                    </Modal>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AppWorks;
